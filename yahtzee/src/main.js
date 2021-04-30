db60edeb74bf1674fbeea5236d960c6971e1d802
Sean <44732696+TheSeanU@users.noreply.github.com>
2021-04-30 18:23:05 +0200

    Update_30-04-2021
    Router,
    Views &
    Components.

--- yahtzee/src/main.js
+++ yahtzee/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+import $ from 'jquery'
+
+window.jQuery = window.$ = $
+require('bootstrap/dist/css/bootstrap.min.css')
+require('bootstrap/dist/js/bootstrap.min.js')
+
+// Vue.config.productionTip = false
 
 new Vue({
-  render: function (h) { return h(App) },
+  router, render: (h) => h(App),
 }).$mount('#app')
---
+++ yahtzee/src/views/Game.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <h3 class="text-center p-4">Yahtzee</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  
+</script>
